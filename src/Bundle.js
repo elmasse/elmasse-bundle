ef7bc9cef377f6dd8750d2b9ec6aa25f3c7bf2c6
Maximiliano Fierro <elmasse@gmail.com>
2015-11-03 14:41:48 -0300

    moved Ext.Base hook back into Bundle file

--- src/Bundle.js
+++ src/Bundle.js
@@ -270,4 +270,26 @@ Ext.define('elmasse.i18n.Bundle', {
         me.callParent();
     }
 
+}, function() {
+    //hook on Ext.Base
+    Ext.override(Ext.Base, {
+        initConfig: function(instanceConfig) {
+            var me = this,
+                cfg = me.getConfigurator(),
+                k;
+
+            me.initConfig = Ext.emptyFn; // ignore subsequent calls to initConfig
+            me.initialConfig = instanceConfig || {};
+
+            for(k in instanceConfig){
+                if(instanceConfig.hasOwnProperty(k) && instanceConfig[k] && typeof instanceConfig[k] === 'object' && instanceConfig[k].type && instanceConfig[k].type === 'bundle'){
+                    instanceConfig[k] = elmasse.i18n.Bundle.instance.getMsg(instanceConfig[k].key);
+                }
+            }
+
+            cfg.configure(me, instanceConfig);
+
+            return me;
+        }
+    });    
 });
